035e4a96f69ecedb3680d58a1b38336a523b64d1
manny-m <manny-m@users.noreply.github.com>
2020-08-26 21:51:38 -0400

    Updates

---
+++ precache-manifest.e56e1a6355486c96e8964a108de31d65.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4943965196b3c5c0f71927a43aacde2",
+    "url": "/mannys-portfolio/index.html"
+  },
+  {
+    "revision": "ea192b1787f15c4204ca",
+    "url": "/mannys-portfolio/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d3b6982b8dba17cecf7b",
+    "url": "/mannys-portfolio/static/js/2.9768367f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/mannys-portfolio/static/js/2.9768367f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea192b1787f15c4204ca",
+    "url": "/mannys-portfolio/static/js/main.a500e248.chunk.js"
+  },
+  {
+    "revision": "46fa0e1ecd0f8c01d9d4",
+    "url": "/mannys-portfolio/static/js/runtime-main.405a87cd.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/mannys-portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
