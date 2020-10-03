7c02d3910a71edea8d26a0c4083900c77d85e533
manny-m <manny-m@users.noreply.github.com>
2020-10-02 23:15:30 -0400

    cutom icon

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import './App.css';
 import Nav from '../nav/Nav';
+import Title from '../title/Title';
 
+const navItems = [
+  {label:'About', color: undefined, active:false},
+  {label:'Experience', color: '#f47c7c', active:false},
+  {label:'Projects', color: '#a1de93', active:false},
+  {label:'Creative', color: '#70a1d7', active:false}
+]
 
 function App() {
   return (
-    <div className="App">
-      <Nav></Nav>
+    <div className='App'>
+      <Title label={'Manuel Moquete'}/>
+      <Nav items={navItems}/>
+      <div className='Apps'>
+        {/* Manuel Moquete  */}
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam metus lorem, commodo non enim in, tincidunt lobortis ex. Nullam ipsum nulla, rhoncus at nibh eu, elementum ultrices arcu. In non suscipit erat. Duis lectus lacus, ultrices id libero id, accumsan finibus ante. Morbi vel dignissim arcu, ut aliquet erat. Phasellus bibendum sodales vehicula. Fusce massa diam, interdum ac faucibus vel, rhoncus vulputate quam. Nam quis posuere augue.
+
+      </div>
     </div>
   );
 }
--- src/components/nav/Nav.js
+++ src/components/nav/Nav.js
@@ -1,24 +1,61 @@
-import React from 'react';
+import React , {useState}from 'react';
 import './Nav.css';
+import { useSpring, animated as a} from 'react-spring';
+import PropTypes from 'prop-types';
+
+const navItemLeftMargin = 50;
+const navItemStartColor = 'rgba(0,0,0,0)'; 
+const navItemDefaultColor = 'grey'; 
 
 const NavItem = (props) =>{
+  const {active, color, label, children} = props;
+  const [hovering, setHovering] = useState(false);
+
+  // When hover slide item by 20
+  const aProps = useSpring({ 
+    paddingLeft: hovering || active ? navItemLeftMargin + 20 : navItemLeftMargin,
+    color: hovering ? color || navItemDefaultColor : navItemStartColor,
+    '-webkit-text-stroke': `1.5px ${hovering ? color || navItemDefaultColor : 'white'}`
+  });
+
   return(
-    <div className="NavItem"> 
-      {props.children}
+    <div 
+      className='NavItem'
+      onMouseEnter={()=>{setHovering(true)}} 
+      onMouseLeave={()=>{setHovering(false)}}
+    >
+      <a.span  style={aProps}> 
+        {label || children}
+      </a.span>
     </div>
   )    
 }
 
+NavItem.propTypes = {
+  color: PropTypes.string,
+  label: PropTypes.string.isRequired,
+  active: PropTypes.bool.isRequired
+};
 
-function Nav() {
+const Nav = (props) => {
+  const { items } = props;
   return (
-    <div className="Nav">
-      <NavItem>About</NavItem>
-      <NavItem>Experience</NavItem>
-      <NavItem>Projects</NavItem>
-      <NavItem>Creative</NavItem>
+    <div className='Nav'>
+      
+      {items.map(item =>
+        <NavItem
+          key={item.label}
+          color={item.color} 
+          active={item.active}
+          label={item.label}
+        />
+      )}
     </div>
   );
 }
+
+Nav.propTypes = { 
+  items: PropTypes.arrayOf(PropTypes.shape(NavItem.propTypes)).isRequired
+};
 
 export default Nav;
